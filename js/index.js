08c06b39cc148ce80c45b812c253badd14d46ec4
garywilki <wil20223@byui.edu>
2021-04-23 21:01:46 -0700

    Portal Home Page

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// Displays "Last Modified" as date and time
+document.querySelector("#lastmodified").textContent = document.lastModified;
+
+// Displays the current year for copyright
+document.querySelector("#theyear").textContent = new Date().getFullYear();
