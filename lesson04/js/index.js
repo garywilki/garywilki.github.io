07c64eeb1ed4ed54b969992b2dc51a20d1500d27
garywilki <wil20223@byui.edu>
2021-05-15 22:47:55 -0700

    Week 04 start

---
+++ lesson04/js/index.js
@@ -0,0 +1,21 @@
+// Displays "Last Modified" as date and time
+document.querySelector("#lastmodified").textContent = document.lastModified;
+
+// Displays the current year for copyright
+document.querySelector("#theyear").textContent = new Date().getFullYear();
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+/*** Programming Notes **************************************
+  Arrow Functions - es6 syntactically compact alternative to a regular function expression
+  see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+  or https://www.w3schools.com/js/js_arrow_function.asp
+
+  classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+  */
